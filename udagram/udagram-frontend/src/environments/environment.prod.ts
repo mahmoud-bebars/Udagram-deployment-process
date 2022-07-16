dea13184349f61ea404249951835d9fc5d4fe9a4
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2022-07-16 20:47:19 +0200

    inital commit 16-7-2022 2047

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
-};
+  appName: 'Udagram',
+  apiHost:
+    'http://udagram-api-env.eba-6uprtkkt.me-south-1.elasticbeanstalk.com/',
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
